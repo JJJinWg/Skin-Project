a76c92c21eda3115d1620b25b31bee058417e125
tear <tear3830@naver.com>
2025-04-04 11:08:24 +0900

    init: reupload project with updated .gitignore

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,24 @@
+// src/screens/HomeScreen.tsx
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>👋 Home Screen 입니다!</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 20,
+  },
+});
